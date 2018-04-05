a5f7c83d7423d7a6c361bb4db7523e446ce1e951
Masaki Ikegami <salvage0707@gmail.com>
2018-04-05 16:14:14 +0900

    finish 3-7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,8 +12,19 @@ class PostsController < ApplicationController
   def index
   	@posts = Post.all
   end
+
   def show
   	@post = Post.find(params[:id])
+  end
+
+  def edit
+  	@post = Post.find(params[:id])
+  end
+
+  def update
+  	@post = Post.find(params[:id])
+  	@post.update(post_params)
+  	redirect_to post_path(@post.id)
   end
 
 
