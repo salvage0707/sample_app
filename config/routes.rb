6e6e2e2cb1a0a67bf0b9dd82d3631b64b52ebacf
Masaki Ikegami <salvage0707@gmail.com>
2018-04-05 15:35:55 +0900

    Finish 3-6

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get '/top' => 'roor#top'
 
   post '/posts' => 'posts#create'
+  get '/posts' => 'posts#index'
+  get '/posts/:id' => 'posts#show', as: 'post'
 end
